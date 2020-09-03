c5e03d75cd3b6841061df9f54aed11196331e8e6
dineshdsv3 <dineshdsv3@gmail.com>
2020-09-03 22:30:21 +0530

    Basics Circle pattern

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { selct } from 'd3';
-
-const data = [25, 30, 45, 60, 20];
+import { select } from 'd3';
 
 function App() {
+	const [data, setData] = useState([25, 30, 45, 60, 20]);
 	const svgRef = useRef();
 
 	useEffect(() => {
-    const svg = select(svgRef.current);
+		const svg = select(svgRef.current);
+		svg.selectAll('circle')
+			.data(data)
+			.join('circle')
+			// .join(
+			// 	(enter) => enter.append('circle').attr('class', 'new'),
+			// 	(update) =>
+			// 		update
+			// 			.attr('class', 'updated')
+			// )
+			.attr('r', (value) => value)
+			.attr('cx', (value) => value * 2)
+			.attr('cy', (value) => value * 2)
+			.attr('stroke', 'red')
+			.attr('fill', 'blue');
+	}, [data]);
 
-  }, []);
-
-	return <svg ref={svgRef}></svg>;
+	return (
+		<React.Fragment>
+			<svg ref={svgRef}></svg>
+			<br />
+			<button onClick={() => setData(data.map((value) => value + 5))}>Update Data</button>
+			<button onClick={() => setData(data.filter((value) => value < 35))}>Filter Data</button>
+		</React.Fragment>
+	);
 }
 
 export default App;
