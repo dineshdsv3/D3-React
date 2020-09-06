e0d55ac1044e8f279b02a4bb34d65b0f5e8d68d1
dineshdsv3 <dineshdsv3@gmail.com>
2020-09-06 13:41:44 +0530

    Added Curved Line chart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { select } from 'd3';
+import { select, line, curveCardinal } from 'd3';
 
 function App() {
-	const [data, setData] = useState([25, 30, 45, 60, 20]);
+	const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
 	const svgRef = useRef();
 
 	useEffect(() => {
 		const svg = select(svgRef.current);
-		svg.selectAll('circle')
-			.data(data)
-			.join('circle')
-			// .join(
-			// 	(enter) => enter.append('circle').attr('class', 'new'),
-			// 	(update) =>
-			// 		update
-			// 			.attr('class', 'updated')
-			// )
-			.attr('r', (value) => value)
-			.attr('cx', (value) => value * 2)
-			.attr('cy', (value) => value * 2)
-			.attr('stroke', 'red')
-			.attr('fill', 'blue');
+		
+		// Curved Line Chart
+		const myLine = line()
+			.x((value, index) => index * 50)
+			.y((value) => 150 - value)
+			.curve(curveCardinal);
+
+		// Circles
+
+		// svg.selectAll('circle')
+		// 	.data(data)
+		// 	.join('circle')
+		// 	// .join(
+		// 	// 	(enter) => enter.append('circle').attr('class', 'new'),
+		// 	// 	(update) =>
+		// 	// 		update
+		// 	// 			.attr('class', 'updated')
+		// 	// )
+		// 	.attr('r', (value) => value)
+		// 	.attr('cx', (value) => value * 2)
+		// 	.attr('cy', (value) => value * 2)
+		// 	.attr('stroke', 'red')
+		// 	.attr('fill', 'blue');
+
+		// Curved Line chart
+
+		svg.selectAll('path')
+			.data([data])
+			.join('path')
+			.attr('d', (value) => myLine(value))
+			.attr('fill', 'none')
+			.attr('stroke', 'blue');
 	}, [data]);
 
 	return (
